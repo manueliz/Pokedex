aa30b205337c1122cfba4b669f838071bf41aac7
Martinisho <mo.gar@outlook.com>
2021-09-12 18:54:39 -0500

    API code finished #1

--- API.js
+++ API.js
@@ -1,3 +1,6 @@
-/*
-insert code here
-*/
+var data;
+function Mypokemon(){
+    data = Math.floor(Math.random() * 800) + 1;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${data}`)
+    .then(data => console.log(data))
+  }
