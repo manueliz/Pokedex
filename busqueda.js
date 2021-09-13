cff7201873b2d82b44120a68fe2297a8bb7aab7d
root <root@DESKTOP-K7IM87M.localdomain>
2021-09-12 20:51:24 -0500

    Busqueda code finished #2

--- busqueda.js
+++ busqueda.js
@@ -1,3 +1,9 @@
+var nombre;
+var resultado;
+function BuscarPoke(){
+    var nombreElement = document.getElementById("nombre");
+    nombre = nombreElement.value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
+    .then(data => console.log(data.json()))
 
-amadeo esta pendejo y shoto
-
+}
